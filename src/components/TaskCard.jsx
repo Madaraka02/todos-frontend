29028b3ef77a02f6b0736a2fa95f1addc6ef916a
Madaraka02 <victormadaraka@gmail.com>
2023-06-20 14:25:47 +0300

    tasKCard design and task tans design

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const TaskCard = () => {
+const TaskCard = ({type, description, end_date}) => {
   return (
-    <div>TaskCard</div>
+    <div className={`font-satoshi w-full rounded-md shadow-md px-4 py-3 cursor-pointer ${type === 'new'? 'border-amber-300' : type === 'progress'? 'border-purple-600' : type === 'complete'? 'border-green-600' : ''} border`}>
+      <p>{description}</p>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import TaskCard from '../components/TaskCard'
 
 function Home() {
   return (
     <>
-    <div className='font-satoshi'>Home</div>
+    <div className="container mx-auto w-full">
+
+    <div className='font-satoshi flex flex-col gap-2 w-full p-6 h-full'>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-1 flex flex-col gap-2">
+          <div className=" flex flex-row cursor-pointer justify-between items-center bg-gray-200 p-2 rounded-md">
+            <p className='font-semibold'>Next Up</p>
+            <div className="rounded-full w-6 h-6 bg-amber-500 justify-center text-center text-white">1</div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <TaskCard type='new'
+            description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            />
+          </div>
+        </div>
+        <div className="col-span-1 flex flex-col gap-2">
+          <div className=" flex flex-row cursor-pointer justify-between items-center bg-gray-200 p-2 rounded-md">
+            <p className='font-semibold'>In Progress</p>
+            <div className="rounded-full w-6 h-6 bg-purple-600 justify-center text-center text-white">1</div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <TaskCard type='progress'
+            description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+            />
+          </div>
+        </div>
+        <div className="col-span-1 flex flex-col gap-2">
+          <div className=" flex flex-row cursor-pointer justify-between items-center bg-gray-200 p-2 rounded-md">
+            <p className='font-semibold'>Complete</p>
+            <div className="rounded-full w-6 h-6 bg-green-600 justify-center text-center text-white">1</div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <TaskCard type='complete'
+            description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+            />
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    <div className="flex px-4">
+      <button className='bg-blue-600 px-4 py-2 rounded-md items-center text-white flex flex-row gap-3 text-[18px] font-satoshi'>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
+    </svg>
+
+        New task</button>
+    </div>
+    </div>
     </>
   )
 }
